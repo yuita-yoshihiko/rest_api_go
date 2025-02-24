0b05bdc63bf781c08674e5df70614240e51e6849
yuita-yoshihiko <soup998211@gmail.com>
2025-02-24 16:13:46 +0900

    dbコネクションコマンドをmain.goに追加

--- cmd/main.go
+++ cmd/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"net/http"
+	"rest_api_go/infrastructure/db"
 	"rest_api_go/infrastructure/router"
 )
 
 func main() {
+	db.Init()
 	http.ListenAndServe(":8089", router.NewRouter())
 }
